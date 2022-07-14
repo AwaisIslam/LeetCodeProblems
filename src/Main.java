cbffd5a16615c68aafe1ca9dc6ae28903d697ab6
AwaisIslam <awais4823@gmail.com>
2022-07-14 16:52:22 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        //Happy Number
+        HappyNumber happyNumber = new HappyNumber();
+        System.out.println(happyNumber.isHappy(20));
+
+        // Single number
+        /*SingleNumber singleNumber = new SingleNumber();
+        int[] nums = {4,1,2,1,2};
+        int result = singleNumber.singleNumber(nums);
+        System.out.println(result);*/
+    }
+}
