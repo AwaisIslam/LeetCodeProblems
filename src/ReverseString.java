b455e94d51c577f47aa20fbe1edfa01aa7588742
AwaisIslam <awais4823@gmail.com>
2022-07-20 17:44:31 +0500

    Updated ReverseString.java

---
+++ src/ReverseString.java
@@ -0,0 +1,16 @@
+public class ReverseString {
+
+    public String reverseString(String s) {
+
+        char[] chars = s.toCharArray();
+        int i = 0;
+        int j = chars.length -1;
+        while (i < j) {
+            char temp = chars[i];
+            chars[i++] = chars[j];
+            chars[j--] = temp;
+        }
+
+        return new String(chars);
+    }
+}
